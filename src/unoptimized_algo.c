f12692260c61e6c83a58f89b9e4601350d2f2763
Markdosso <markdosso9@gmail.com>
2021-07-19 12:36:36 -0700

    Added code comments on how neon intrinsics would work

--- src/unoptimized_algo.c
+++ src/unoptimized_algo.c
@@ -14,21 +14,13 @@ int unoptimized_algo(int input[8][8], int output[8][8]) {
   int x, y;
   for(y = 2; y != 0; y--) {
     for(x = 0; x < 8; x++) {
-<<<<<<< HEAD
-      int temp1;
-      int temp2;
-      int temp3;
-      int temp4;
-=======
       int temp1, temp2, temp3, temp4;
 
->>>>>>> da03ec864ea403eb14d9a300688082ecbd90662c
       temp1 = input[0][x] + input[7][x];
       temp2 = input[1][x] + input[6][x];
       temp3 = input[2][x] + input[5][x];
       temp4 = input[3][x] + input[4][x];
 
-<<<<<<< HEAD
       /*
       * 
       * int temp[4];
@@ -91,12 +83,6 @@ int unoptimized_algo(int input[8][8], int output[8][8]) {
       output[x][2] = ((C2 * temp1) +  (C6 * temp2) + (-C6 * temp3) + (-C2 * temp4)) >> 15;
       output[x][4] = ((C4 * temp1) +  (-C4 * temp2) + (-C4 * temp3) + (C4 * temp4)) >> 15;
       output[x][6] = ((C6 * temp1) +  (-C2 * temp2) + (C2 * temp3) + (-C6 * temp4)) >> 15;
-=======
-      output[x][0] = ((C4 * temp1) + (C4 * temp2) + (C4 * temp3) + (C4 * temp4)) >> 15;
-      output[x][2] = ((C2 * temp1) + (C6 * temp2) + (-C6 * temp3) + (-C2 * temp4)) >> 15;
-      output[x][4] = ((C4 * temp1) + (-C4 * temp2) + (-C4 * temp3) + (C4 * temp4)) >> 15;
-      output[x][6] = ((C6 * temp1) + (-C2 * temp2) + (C2 * temp3) + (-C6 * temp4)) >> 15;
->>>>>>> da03ec864ea403eb14d9a300688082ecbd90662c
 
       temp1 = input[0][x] - input[7][x];
       temp2 = input[1][x] - input[6][x];
