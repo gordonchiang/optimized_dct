e8b2c86d9a0358c9250a41626c43eb2304f771fc
Gordon <gchiang@uvic.ca>
2021-07-24 02:05:46 -0700

    Slight optimization of loop

--- src/unrolled.c
+++ src/unrolled.c
@@ -14,7 +14,7 @@ void unrolled(int32_t input[8][8], int32_t output[8][8]) {
   register int64_t temp1, temp2, temp3, temp4;
   int64_t temp[8][8];
 
-  for (i = 0; i < 8; i++) {
+  for (i ^= i; !(i&8); i++) {
     temp1 = input[i][0] + input[i][7];
     temp2 = input[i][1] + input[i][6];
     temp3 = input[i][2] + input[i][5];
@@ -37,7 +37,7 @@ void unrolled(int32_t input[8][8], int32_t output[8][8]) {
     temp[7][i] = ((C7 * temp1) + (-C5 * temp2) + (C3 * temp3) + (-C1 * temp4));
   }
 
-  for(i = 0; i < 8; i++) {
+  for(i ^= i; !(i&8); i++) {
     temp1 = temp[i][0] + temp[i][7];
     temp2 = temp[i][1] + temp[i][6];
     temp3 = temp[i][2] + temp[i][5];
