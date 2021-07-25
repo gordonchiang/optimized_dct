cb6b471c6a491bfe2a429cdaa6f6890790969748
Gordon <gchiang@uvic.ca>
2021-07-25 00:02:34 -0700

    Lower C scale factor to 8

--- src/unrolled.c
+++ src/unrolled.c
@@ -1,13 +1,13 @@
 #include <stdint.h>
 
-// Scale factor << 14
-#define C1 16069
-#define C2 15137
-#define C3 13623
-#define C4 11585
-#define C5 9102
-#define C6 6270
-#define C7 3196
+// Scale factor << 8
+#define C1 251
+#define C2 237
+#define C3 213
+#define C4 181
+#define C5 142
+#define C6 98
+#define C7 50
 
 // This algorithm builds on top of UNOPTIMIZED
 // It unrolls the loop
@@ -28,20 +28,20 @@ void unrolled(int32_t input[8][8], int32_t output[8][8]) {
     temp4 = input[i][3] + input[i][4];
 
     // Maintain transpose-ness
-    temp[0][i] = (temp1 + temp2 + temp3 + temp4) * C4 >> 14;
-    temp[2][i] = ((C2 * temp1) + (C6 * temp2) + (-C6 * temp3) + (-C2 * temp4)) >> 14;
-    temp[4][i] = ((temp1 + temp4) * C4 + (temp2 + temp3) * -C4) >> 14;
-    temp[6][i] = ((C6 * temp1) + (-C2 * temp2) + (C2 * temp3) + (-C6 * temp4)) >> 14;
+    temp[0][i] = (temp1 + temp2 + temp3 + temp4) * C4;
+    temp[2][i] = ((C2 * temp1) + (C6 * temp2) + (-C6 * temp3) + (-C2 * temp4));
+    temp[4][i] = ((temp1 + temp4) * C4 + (temp2 + temp3) * -C4);
+    temp[6][i] = ((C6 * temp1) + (-C2 * temp2) + (C2 * temp3) + (-C6 * temp4));
 
     temp1 = input[i][0] - input[i][7];
     temp2 = input[i][1] - input[i][6];
     temp3 = input[i][2] - input[i][5];
     temp4 = input[i][3] - input[i][4];
 
-    temp[1][i] = ((C1 * temp1) + (C3 * temp2) + (C5 * temp3) + (C7 * temp4)) >> 14;
-    temp[3][i] = ((C3 * temp1) + (-C7 * temp2) + (-C1 * temp3) + (-C5 * temp4)) >> 14;
-    temp[5][i] = ((C5 * temp1) + (-C1 * temp2) + (C7 * temp3) + (C3 * temp4)) >> 14;
-    temp[7][i] = ((C7 * temp1) + (-C5 * temp2) + (C3 * temp3) + (-C1 * temp4)) >> 14;
+    temp[1][i] = ((C1 * temp1) + (C3 * temp2) + (C5 * temp3) + (C7 * temp4));
+    temp[3][i] = ((C3 * temp1) + (-C7 * temp2) + (-C1 * temp3) + (-C5 * temp4));
+    temp[5][i] = ((C5 * temp1) + (-C1 * temp2) + (C7 * temp3) + (C3 * temp4));
+    temp[7][i] = ((C7 * temp1) + (-C5 * temp2) + (C3 * temp3) + (-C1 * temp4));
   }
 
   for(i ^= i; !(i&8); i++) {
@@ -51,19 +51,21 @@ void unrolled(int32_t input[8][8], int32_t output[8][8]) {
     temp4 = temp[i][3] + temp[i][4];
 
     // Transpose again to get original image
-    output[i][0] = ((temp1 + temp2 + temp3 + temp4) * C4) >> 16;
-    output[i][2] = ((C2 * temp1) + (C6 * temp2) + (-C6 * temp3) + (-C2 * temp4)) >> 16;
-    output[i][4] = ((temp1 + temp4) * C4 + (temp2 + temp3) * -C4) >> 16;
-    output[i][6] = ((C6 * temp1) + (-C2 * temp2) + (C2 * temp3) + (-C6 * temp4)) >> 16;
+    // Divide each output element by SF
+    // Divide by an additional factor of 4 (>> 2)
+    output[i][0] = ((temp1 + temp2 + temp3 + temp4) * C4) >> 18;
+    output[i][2] = ((C2 * temp1) + (C6 * temp2) + (-C6 * temp3) + (-C2 * temp4)) >> 18;
+    output[i][4] = ((temp1 + temp4) * C4 + (temp2 + temp3) * -C4) >> 18;
+    output[i][6] = ((C6 * temp1) + (-C2 * temp2) + (C2 * temp3) + (-C6 * temp4)) >> 18;
 
     temp1 = temp[i][0] - temp[i][7];
     temp2 = temp[i][1] - temp[i][6];
     temp3 = temp[i][2] - temp[i][5];
     temp4 = temp[i][3] - temp[i][4];
 
-    output[i][1] = ((C1 * temp1) + (C3 * temp2) + (C5 * temp3) + (C7 * temp4)) >> 16;
-    output[i][3] = ((C3 * temp1) + (-C7 * temp2) + (-C1 * temp3) + (-C5 * temp4)) >> 16;
-    output[i][5] = ((C5 * temp1) + (-C1 * temp2) + (C7 * temp3) + (C3 * temp4)) >> 16;
-    output[i][7] = ((C7 * temp1) + (-C5 * temp2) + (C3 * temp3) + (-C1 * temp4)) >> 16;
+    output[i][1] = ((C1 * temp1) + (C3 * temp2) + (C5 * temp3) + (C7 * temp4)) >> 18;
+    output[i][3] = ((C3 * temp1) + (-C7 * temp2) + (-C1 * temp3) + (-C5 * temp4)) >> 18;
+    output[i][5] = ((C5 * temp1) + (-C1 * temp2) + (C7 * temp3) + (C3 * temp4)) >> 18;
+    output[i][7] = ((C7 * temp1) + (-C5 * temp2) + (C3 * temp3) + (-C1 * temp4)) >> 18;
   }
 }
