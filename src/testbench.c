71a6e3265770c9f5ab354be7ad51ef4e6ae12f98
Gordon <gchiang@uvic.ca>
2021-08-09 23:25:02 -0700

    Lint

--- src/testbench.c
+++ src/testbench.c
@@ -22,15 +22,15 @@ uint8_t *read(char *filepath, size_t dimensions) {
 }
 
 int checkBlock(int output[8][8], int output2[8][8]){
-    int i, j;
-    for(i = 0; i < 8; i++) {
-        for(j = 0; j < 8; j++) {
-            if(output[i][j] > (output2[i][j] + 2) || output[i][j] < (output2[i][j] - 2)) {
-                return EXIT_FAILURE;
-            }
-        }
+  int i, j;
+  for(i = 0; i < 8; i++) {
+    for(j = 0; j < 8; j++) {
+      if(output[i][j] > (output2[i][j] + 2) || output[i][j] < (output2[i][j] - 2)) {
+        return EXIT_FAILURE;
+      }
     }
-    return EXIT_SUCCESS;
+  }
+  return EXIT_SUCCESS;
 }
 
 void print_matrix1(int matrix[8][8], int width, int height) {
@@ -45,53 +45,50 @@ void print_matrix1(int matrix[8][8], int width, int height) {
 }
 
 int testcase(char *input_file, int width, int height) {
-    uint8_t *image = read(input_file, width*height);
-    int i, j, k, l;
-    int width_offset = width / 8;
-    int height_offset = height / 8;
+  uint8_t *image = read(input_file, width*height);
+  int i, j, k, l;
+  int width_offset = width / 8;
+  int height_offset = height / 8;
 
-    for (i = 0; i < height_offset; i++) {
-        for (j = 0; j < width_offset; j++) {
-            int block[8][8];
-            int block2[8][8];
-            int output[8][8];
-            int output2[8][8];
+  for (i = 0; i < height_offset; i++) {
+    for (j = 0; j < width_offset; j++) {
+      int block[8][8];
+      int block2[8][8];
+      int output[8][8];
+      int output2[8][8];
 
-            for (k = 0; k < 8; k++) {
-                for (l = 0; l < 8; l++) {
-                    int index = width*((i*8) + k) + ((j*8) + l);
-                    block[k][l] = image[index];
-                    block2[k][l] = image[index];
-                }
-            }
+      for (k = 0; k < 8; k++) {
+        for (l = 0; l < 8; l++) {
+          int index = width*((i*8) + k) + ((j*8) + l);
+          block[k][l] = image[index];
+          block2[k][l] = image[index];
+        }
+      }
 
-            neon(block, output); // change to what method will be the fully optimized algo
-            naive(block2, output2);
+      neon(block, output); // change to what method will be the fully optimized algo
+      naive(block2, output2);
 
-            if(checkBlock(output, output2) == 1) {
-                return EXIT_FAILURE;
-            }
-        } 
-    } 
-    return EXIT_SUCCESS;  
+      if(checkBlock(output, output2) == 1) {
+        return EXIT_FAILURE;
+      }
+    }
+  } 
+  return EXIT_SUCCESS;  
 }
 
-
-
 int main(int argc, char *argv[]) {
-    
-    if (argc != 2) {
-        printf("Error: invalid number of arguments (example: ./main ./test/8x8_64_byte)\n");
-        return EXIT_FAILURE;
-    }
-    
-    char *input_file = argv[1]; // "./test/testimage.png",
-    
-    if(testcase(input_file, 320, 240) == 0) {
-        printf("testcase Success\n");
-    } else {
-        printf("testcase Failure\n");
-    }
-    
-    return EXIT_SUCCESS;
+  if (argc != 2) {
+    printf("Error: invalid number of arguments (example: ./main ./test/8x8_64_byte)\n");
+    return EXIT_FAILURE;
+  }
+  
+  char *input_file = argv[1]; // "./test/testimage.png",
+  
+  if(testcase(input_file, 320, 240) == 0) {
+    printf("testcase Success\n");
+  } else {
+    printf("testcase Failure\n");
+  }
+  
+  return EXIT_SUCCESS;
 }
